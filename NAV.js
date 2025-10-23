53cc50e3a07ef03f10d0bda87c4c03baa31be96a
ENwabudu1 <ENwabudu@lions.molloy.edu>
2025-10-23 02:55:20 -0400

    Create NAV.js

---
+++ NAV.js
@@ -0,0 +1,7 @@
+<nav>
+          <ul>
+            <li><HashLink to = "/">Home</HashLink></li>
+            <li><HashLink to = "/about">About Organization</HashLink></li>
+            <li><HashLink to = "/contact">Contact Us</HashLink></li>
+          </ul>
+        </nav>
